a4e3c5acdf55527b747aaaeb6ee04b3921fc0928
mudkannaa <ashwinimudknna@gmail.com>
2023-06-23 11:05:39 +0530

    add admin

---
+++ src/pages/Tables.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-no-target-blank */
+import PageTitle from "../components/PageTitle";
+import LayoutCard from "./../layout/LayoutCard";
+import Table from "../components/tables/Table";
+function Tables() {
+  return (
+    <>
+      <PageTitle title="Tables">
+        DataTables is a third party plugin that is used to generate the demo
+        table below. For more information about DataTables, please visit the{" "}
+        <a target="_blank" href="https://datatables.net">
+          official DataTables documentation
+        </a>
+        .
+      </PageTitle>
+      <LayoutCard title="DataTables Example">
+        <Table />
+      </LayoutCard>
+    </>
+  );
+}
+
+export default Tables;
